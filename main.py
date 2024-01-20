05f1e3a88ead542b3832da478f44d027a0c650c8
Alexandr Kovalev <alex_kov@inbox.ru>
2024-01-21 00:05:48 +0500

    Finish HW

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+from pprint import pprint
+
 from bs4 import BeautifulSoup as bs
 import requests
 import fake_headers
-import pprint
+import json
+
 
 url = "https://hh.ru/search/vacancy"
 params = {
-    'area': '1',
-    'text': 'Django'
+    'area': ['1', '2'],
+    'text': ['Django', 'Flask'],
+    'per_page': '100',
+    'customDomain': '1'
 }
 
 
@@ -16,12 +21,30 @@ def gen_headers():
 
 
 def get_txt(source):
-    response = requests.get(source, headers=gen_headers(), params=params).text
-    soup = bs(response, 'lxml')
+    data = dict()
+    response = requests.get(source, headers=gen_headers(), params=params)
+    soup = bs(response.text, 'lxml')
     all_frame = soup.find_all('div', class_='serp-item')
     for frame in all_frame:
-        soup.find('div', class_='vacancy-serp-item__layout')
+        try:
+            header = frame.find('span', class_='serp-item__title').text
+            salary = frame.find('span', class_='bloko-header-section-2').text.replace('\u202f', " ")
+            link = frame.find('a', class_='bloko-link').get('href')
+            company_name = frame.find('a', class_='bloko-link bloko-link_kind-tertiary').text.replace('\xa0', " ")
+            sity = frame.find('div', {'data-qa': 'vacancy-serp__vacancy-address'}).text.strip()
+            data[header] = []
+            data[header].append({
+                'salary': salary,
+                'url': link,
+                'company': company_name,
+                'sity': sity
+            })
+        except AttributeError:
+            continue
 
-        print(frame.text)
+    return data
 
-get_txt(url)
+
+with open('data.json', 'w', encoding='utf-8') as f:
+    json.dump(get_txt(url), f, ensure_ascii=False)
+
